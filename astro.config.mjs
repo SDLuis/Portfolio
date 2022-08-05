9eb393635131a2a52b0b2d233c5d37e36daf170b
Luis <Luistejeda0011@gmail.com>
2022-08-05 20:33:14 -0300

    Changing to Yarn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
-  integrations: [
-    tailwind(),
-    react(),
-  ],
+  integrations: [tailwind(), react()],
+  adapter: netlify()
 });
