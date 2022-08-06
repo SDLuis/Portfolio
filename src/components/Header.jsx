4e9a6a624b7db43130e60d9b19e538d358746c90
Luis <Luistejeda0011@gmail.com>
2022-08-06 01:10:09 -0300

    Removing attributes that do not need yet and changing animation times

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import { content } from "@content/index";
 import Typical from "react-typical";
-import { LazyLoadImage, trackWindowScroll } from "react-lazy-load-image-component";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-function Header() {
+export default function Header() {
   return (
     <div className=" min-h-screen grid place-items-center">
       <div className=" w-7/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between">
@@ -17,7 +17,7 @@ function Header() {
         <div className="grid place-items-center">
           <h2
             className={
-              "transform transition duration-2000 mt-4 sm:mt-0 ease-in-out text-2xl md:text-5xl font-bold text-white"
+              "mt-4 sm:mt-0 text-2xl md:text-5xl font-bold text-white"
             }
           >
             {content.header.text[0]}
@@ -26,7 +26,7 @@ function Header() {
           </h2>
           <h1
             className={
-              "transform transition duration-2000 ease-in-out mt-2 font-bold text-xl sm:text-2xl md:text-3xl text-gray-400"
+              "mt-2 font-bold text-xl sm:text-2xl md:text-3xl text-gray-400"
             }
           >
             {content.header.text[2]}{" "}
@@ -45,4 +45,4 @@ function Header() {
   );
 }
 
-export default trackWindowScroll(Header)
+
