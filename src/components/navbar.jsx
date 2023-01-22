2ff8c341846cd94101a85b5d3079d5da982ae98d
Luis <Luistejeda0011@gmail.com>
2023-01-22 17:39:21 -0400

    Less space between buttons on navbar "Mobile screen"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         <IconMenu />
       </label>
       <nav className="absolute h-screen lg:h-auto top-0 left-0 right-0 z-40 hidden min-h-screen py-24 bg-white peer-checked:block lg:block lg:static lg:bg-inherit lg:py-0 lg:min-h-0 bg-opacity-40 backdrop-blur-lg lg:backdrop-blur-0">
-        <ul className="grid place-items-center lg:flex h-[80%] lg:h-auto lg:items-start lg:flex-row font-semibold tracking-tight text-xl gap-x-8 text-white [&>li>a]:border-b-2 [&>li>a]:border-b-transparent [&>li>a]:transition-all gap-y-4">
+        <ul className="grid place-items-center lg:flex h-[60%] lg:h-auto lg:items-start lg:flex-row font-semibold tracking-tight text-xl gap-x-8 text-white [&>li>a]:border-b-2 [&>li>a]:border-b-transparent [&>li>a]:transition-all gap-y-4">
           <li className="flex">
             <a href="/" className="relative hover:border-b-black">
               Home
